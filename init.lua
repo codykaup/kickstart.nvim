1a0342547b8694e012d5a95f09383756da62559b
Cody Kaup <dbkaup@gmail.com>
2024-04-29 10:50:11 -0500

    Add language servers

--- init.lua
+++ init.lua
@@ -569,7 +569,19 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        -- Go servers
+        gopls = {},
+
+        -- JS/TS servers
+        tsserver = {},
+        eslint = {},
+        prettier = {},
+
+        -- Utilities
+        jsonls = {},
+        terraformls = {},
+        yamlls = {},
+
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
