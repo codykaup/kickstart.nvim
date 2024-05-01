fe642869ef172c16e6a716f562ba5a19c85572f0
Cody Kaup <dbkaup@gmail.com>
2024-05-01 16:32:21 -0500

    Add shortcuts from previous config

--- init.lua
+++ init.lua
@@ -158,6 +158,11 @@ vim.opt.scrolloff = 10
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+vim.keymap.set('n', 'zi', '<C-w>_| <C-w>|', { desc = 'Maximize current split' })
+vim.keymap.set('n', 'zo', '<C-w>=', { desc = 'Equalize split sizes' })
+vim.keymap.set('v', '<leader>y', '"*y', { desc = 'Yank to system clipboard' })
+vim.keymap.set('n', '<leader>p', '"*p', { desc = 'Paste from system clipboard' })
+
 -- Set jk as a quick escape from insert mode
 vim.keymap.set('i', 'jk', '<Esc>')
 
@@ -170,6 +175,12 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Vim keymaps
+vim.keymap.set('n', '<leader>gs', ':tab Git<CR>', { desc = '[G]it [S]tatus' })
+vim.keymap.set('n', '<leader>gc', ":Git commit -m ''<left>", { desc = '[G]it [C]ommit' })
+vim.keymap.set('n', '<leader>gb', ':Git blame<CR><C-w>l', { desc = '[G]it [B]lame' })
+vim.keymap.set('n', '<leader>gl', ':Git log', { desc = '[G]it [L]og' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
