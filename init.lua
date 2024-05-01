6b4b0238fc669c27a0458c4339c4a4682de47100
Cody Kaup <dbkaup@gmail.com>
2024-05-01 16:37:22 -0500

    Add GH copilot

--- init.lua
+++ init.lua
@@ -924,6 +924,8 @@ require('lazy').setup({
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
+  'github/copilot.vim',
+
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
